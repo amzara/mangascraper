3e4ef5825e6a6ba0dbec6a6cf45d838aaeab3f6b
Amzara <amzaraxt@gmail.com>
2026-01-24 23:19:37 +0800

    headers

--- go.mod
+++ go.mod
@@ -9,15 +9,21 @@ require (
 	github.com/antchfx/xmlquery v1.5.0 // indirect
 	github.com/antchfx/xpath v1.3.5 // indirect
 	github.com/bits-and-blooms/bitset v1.24.4 // indirect
+	github.com/deckarep/golang-set/v2 v2.7.0 // indirect
+	github.com/go-jose/go-jose/v3 v3.0.4 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gocolly/colly v1.2.0 // indirect
 	github.com/gocolly/colly/v2 v2.3.0 // indirect
 	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/lib4u/fake-useragent v1.0.6 // indirect
 	github.com/nlnwa/whatwg-url v0.6.2 // indirect
+	github.com/playwright-community/playwright-go v0.5200.1 // indirect
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
+	golang.org/x/exp v0.0.0-20250106191152-7588d65b2ba8 // indirect
 	golang.org/x/net v0.49.0 // indirect
 	golang.org/x/text v0.33.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
--- main.go
+++ main.go
@@ -16,7 +16,28 @@ func main() {
 
 	c.OnRequest(func(r *colly.Request) {
 		r.Headers.Set("Referer", "https://www.mangakakalot.gg/")
-
+		r.Headers.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+		r.Headers.Set("Accept-Encoding", "gzip, deflate, br, zstd")
+		r.Headers.Set("Accept-Language", "en-CA,en-GB;q=0.9,en-US;q=0.8,en;q=0.7")
+		r.Headers.Set("Cookie", "__cflb=02DiuD2F3RAeJGVVKWvfHx9L6dXK13KrhbngRPZNy7fAs; cf_clearance=Zq0AR_5jvoYeFUe2zXcLd0OVt_PCuzRD4ofTRrJE6RI-1769264208-1.2.1.1-_DHlmlmh1DND8Nv.O2CUlThblzA2f5gFm..Dbqb6KP_2I7bjUgjO71fsnq.lGT0oetC5sGwKAN_GsD9Or4B2lzODiT2cKdznkhuE638oGOHo7lrwnwy9dYrM8bOJNPPYPRLcTfLA3zeGGy8lpxcpKvwvky.Fm7MmO.RRAl7gnxQFnkr2nV3iG33l58qtCPvlqVUzC7ZquXb7QlLtoYwk6X.BCozmcSZBF6q4fjm37nw; _pu_clickadu=1769264273381; _pu_last=monetag; _pu_monetag=1769264768380")
+		r.Headers.Set("Dnt", "1")
+		r.Headers.Set("If-Modified-Since", "Sat, 24 Jan 2026 14:22:37 GMT")
+		r.Headers.Set("Priority", "u=0, i")
+		r.Headers.Set("Sec-Ch-Ua", `"Google Chrome";v="143", "Chromium";v="143", "Not A(Brand";v="24"`)
+		r.Headers.Set("Sec-Ch-Ua-Arch", `"x86"`)
+		r.Headers.Set("Sec-Ch-Ua-Bitness", `"64"`)
+		r.Headers.Set("Sec-Ch-Ua-Full-Version", `"143.0.7499.169"`)
+		r.Headers.Set("Sec-Ch-Ua-Full-Version-List", `"Google Chrome";v="143.0.7499.169", "Chromium";v="143.0.7499.169", "Not A(Brand";v="24.0.0.0"`)
+		r.Headers.Set("Sec-Ch-Ua-Mobile", `?0`)
+		r.Headers.Set("Sec-Ch-Ua-Model", `""`)
+		r.Headers.Set("Sec-Ch-Ua-Platform", `"Linux"`)
+		r.Headers.Set("Sec-Ch-Ua-Platform-Version", `""`)
+		r.Headers.Set("Sec-Fetch-Dest", "document")
+		r.Headers.Set("Sec-Fetch-Mode", "navigate")
+		r.Headers.Set("Sec-Fetch-Site", "none")
+		r.Headers.Set("Sec-Fetch-User", "?1")
+		r.Headers.Set("Upgrade-Insecure-Requests", "1")
+		r.Headers.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36")
 	})
 
 	//anonymous function, callback, closure
@@ -31,5 +52,5 @@ func main() {
 		fmt.Println("Visiting", r.URL)
 	})
 
-	c.Visit("https://www.mangakakalot.gg/manga/rust/chapter-37")
+	c.Visit("https://www.mangakakalot.gg")
 }
